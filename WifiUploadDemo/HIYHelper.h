7fefb75626577450073f3ab6709886303f6a199d
Donald <donald@DonalddeMacBook-Air.local>
2017-04-11 17:34:15 +0800

    添加

---
+++ WifiUploadDemo/HIYHelper.h
@@ -0,0 +1,20 @@
+//
+//  HIYHelper.h
+//  IOSProjectDemo
+//
+//  Created by Donald on 17/4/11.
+//  Copyright © 2017年 Susu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HIYHelper : NSObject
+
+//返回本机的IP地址
++ (NSString *)getLocalAddress;
+
++ (NSString *)getWifiName;
+
+
+
+@end
---
+++ WifiUploadDemo/HIYHelper.m
@@ -0,0 +1,13 @@
+//
+//  HIYHelper.m
+//  IOSProjectDemo
+//
+//  Created by Donald on 17/4/11.
+//  Copyright © 2017年 Susu. All rights reserved.
+//
+
+#import "HIYHelper.h"
+
+@implementation HIYHelper
+
+@end
