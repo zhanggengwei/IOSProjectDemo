6c2e7144892a78cdf8ac7685eea09dbfcb3cfde7
Donald <donald@DonalddeMacBook-Air.local>
2017-04-11 17:58:04 +0800

    上传IP地址的获取的代码

--- WifiUploadDemo/HIYHelper.m
+++ WifiUploadDemo/HIYHelper.m
@@ -7,7 +7,45 @@
 //
 
 #import "HIYHelper.h"
+#import <netdb.h>
+#import <sys/socket.h>
+#import <netinet/in.h>
+#import <arpa/inet.h>
+#import <unistd.h>
+#import <ifaddrs.h>
 
 @implementation HIYHelper
++ (NSString *)getLocalAddress
+{
+    struct ifaddrs * interface = NULL;
+    struct ifaddrs * temp = interface;
+    int flag = getifaddrs(&interface);
+    if(flag==0)
+    {
+        while (temp)
+        {
+            
+            if(temp->ifa_addr->sa_family == AF_INET)
+            {
+                //char		*inet_ntoa(struct in_addr);
+                 struct sockaddr_in * address = (struct sockaddr_in *)temp->ifa_addr;
+                 char * addressString = inet_ntoa(address->sin_addr);
+            }
+            temp = temp->ifa_next;
+        }
+        if(interface)
+        {
+            freeifaddrs(interface);
+            interface = NULL;
+        }
+    }
+    return @"";
+    
+    
+}
 
++ (NSString *)getWifiName
+{
+    
+}
 @end
