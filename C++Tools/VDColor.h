f41d0f14c0212f2115460f9db7e72adcc3256c5e
Donald <donald@DonalddeMacBook-Air.local>
2017-04-24 10:26:00 +0800

    c++工具类实现

---
+++ C++Tools/VDColor.h
@@ -0,0 +1,25 @@
+//
+//  VDColor.hpp
+//  IOSProjectDemo
+//
+//  Created by Donald on 17/4/24.
+//  Copyright © 2017年 Susu. All rights reserved.
+//
+
+#ifndef VDColor_hpp
+#define VDColor_hpp
+
+#include <stdio.h>
+#include <iostream>
+class VDColor
+{
+public:
+    VDColor(int value);
+    
+private:
+    
+};
+
+
+
+#endif /* VDColor_hpp */
