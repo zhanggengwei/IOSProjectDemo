a5457f4d131cf09baf37ece20e683172578b1a98
Donald <donald@DonalddeMacBook-Air.local>
2017-04-24 14:41:40 +0800

    c++ objectc混编代码

--- IOSC++Demo/VDImage.mm
+++ IOSC++Demo/VDImage.mm
@@ -8,11 +8,15 @@
 
 #include "VDImage.hpp"
 #include <iostream>
+#include <Foundation/Foundation.h>
+
 using namespace std;
 VDImage::VDImage(char * fileName)
 {
     this->file = fopen(fileName,"rb");
     this->filePath = fileName;
+    this->image = [UIImage imageNamed:@"签名"];
+
 }
 void VDImage::printFileContent()
 {
