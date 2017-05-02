0176e1593a4a7cac0b59d295af50b044a59cb8ba
Donald <donald@DonalddeMacBook-Air.local>
2017-05-02 16:56:11 +0800

    添加 realm

---
+++ IOSRealm-OC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOSRealm-OC
+//
+//  Created by Donald on 17/5/2.
+//  Copyright © 2017年 Susu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOSRealm-OC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  IOSRealm-OC
+//
+//  Created by Donald on 17/5/2.
+//  Copyright © 2017年 Susu. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <realm>
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
