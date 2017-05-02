279b072e400475b1c009a9809eb3401b87aaae7b
Donald <donald@DonalddeMacBook-Air.local>
2017-05-02 13:32:55 +0800

    添加realm数据库的文件

---
+++ IOSRealm/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  IOSRealm
+//
+//  Created by Donald on 17/5/2.
+//  Copyright © 2017年 Susu. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-IOSRealm/Pods-IOSRealm-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IOSRealm : NSObject
+@end
+@implementation PodsDummy_Pods_IOSRealm
+@end
