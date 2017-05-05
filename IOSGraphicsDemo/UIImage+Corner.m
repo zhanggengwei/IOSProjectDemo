f96de2fcbf5dd6f3b18348cf6c8f54dd3351b208
Donald <donald@DonalddeMacBook-Air.local>
2017-05-05 15:59:41 +0800

    commit

--- IOSGraphicsDemo/UIImage+Corner.m
+++ IOSGraphicsDemo/UIImage+Corner.m
@@ -11,19 +11,40 @@
 @implementation UIImage (Corner)
 - (UIImage *)cornerImage
 {
-    UIGraphicsBeginImageContext(self.size);
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    CGRect rect = CGRectMake(0,0,self.size.width,self.size.height);
+//    UIGraphicsBeginImageContext(self.size);
+//    CGContextRef context = UIGraphicsGetCurrentContext();
+//    CGRect rect = CGRectMake(0,0,self.size.width,self.size.height);
+//    
+//    CGContextTranslateCTM(context,0,self.size.height);
+////    CGContextRotateCTM(context, M_PI);
+//    CGContextScaleCTM(context, 1.0, -1.0);
+//     CGContextSetBlendMode(context, kCGBlendModeNormal);
+//    CGContextClipToMask(context, rect, self.CGImage);
+//    [[UIColor blueColor] setFill];
+//    CGContextFillRect(context, rect);
+//    UIImage * image = UIGraphicsGetImageFromCurrentImageContext();
+//    UIGraphicsEndImageContext();
+//    return image;
     
-    CGContextTranslateCTM(context,0,self.size.height);
-//    CGContextRotateCTM(context, M_PI);
-    CGContextScaleCTM(context, 1.0, -1.0);
-     CGContextSetBlendMode(context, kCGBlendModeNormal);
-    CGContextClipToMask(context, rect, self.CGImage);
-    [[UIColor blueColor] setFill];
-    CGContextFillRect(context, rect);
-    UIImage * image = UIGraphicsGetImageFromCurrentImageContext();
+    return [self imageWithTintColor:[UIColor yellowColor]];
+    
+}
+- (UIImage *) imageWithTintColor:(UIColor *)tintColor
+{
+    //We want to keep alpha, set opaque to NO; Use 0.0f for scale to use the scale factor of the device’s main screen.
+    UIGraphicsBeginImageContextWithOptions(self.size, NO, 0.0f);
+    CGRect bounds = CGRectMake(0, 0, self.size.width, self.size.height);
+    
+    [tintColor setFill];
+  
+    UIRectFill(bounds);
+    [self drawInRect:bounds blendMode:kCGBlendModeNormal alpha:1.0f];
+    
+    //Draw the tinted image in context
+    
+    UIImage *tintedImage = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
-    return image;
+    
+    return tintedImage;
 }
 @end
