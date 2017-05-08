5681a6602fdd0ffbbe139a36ce624d2aa0a1cf75
Donald <donald@DonalddeMacBook-Air.local>
2017-05-08 15:39:19 +0800

    添加动画的实现

--- IOSLoadingDemo/ViewController.m
+++ IOSLoadingDemo/ViewController.m
@@ -16,8 +16,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    CAShapeLayer * layer = [CAShapeLayer layer];
+    layer.bounds = CGRectMake(0, 0, 120, 120);
+    layer.path = [UIBezierPath bezierPathWithArcCenter:CGPointMake(60, 60) radius:65 startAngle:0 endAngle:M_PI * 2 clockwise:YES].CGPath;
+    layer.fillColor = [UIColor clearColor].CGColor;
+    layer.strokeColor = [UIColor yellowColor].CGColor;
+    
+    layer.position = CGPointMake(130, 130);
+    [self.view.layer addSublayer:layer];
+    
     TriangleView * view = [[TriangleView alloc]initWithFrame:CGRectMake(100, 100, 50, 50)];
     [self.view addSubview:view];
+    
+    CABasicAnimation * animation = [CABasicAnimation new];
+    animation.keyPath = @"strokeEnd";
+    animation.fromValue = @0;
+    animation.toValue = @1;
+    animation.duration = 3;
+    animation.repeatCount = 10000;
+    animation.removedOnCompletion = YES;
+    animation.fillMode = kCAFillModeForwards;
+    [layer addAnimation:animation forKey:@""];
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     // Do any additional setup after loading the view, typically from a nib.
 }
