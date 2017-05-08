ee5548ecef0fa1f84190fea1312928cf44f4b093
Donald <donald@DonalddeMacBook-Air.local>
2017-05-08 11:43:55 +0800

    模仿dock界面滑动

---
+++ TableViewIndexDemo/PPTIndexView.h
@@ -0,0 +1,15 @@
+//
+//  PPTIndexView.h
+//  IOSProjectDemo
+//
+//  Created by Donald on 17/5/8.
+//  Copyright © 2017年 Susu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PPTIndexView : UIView
+
+- (instance)init
+
+@end
---
+++ TableViewIndexDemo/PPTIndexView.m
@@ -0,0 +1,17 @@
+//
+//  PPTIndexView.m
+//  IOSProjectDemo
+//
+//  Created by Donald on 17/5/8.
+//  Copyright © 2017年 Susu. All rights reserved.
+//
+
+#import "PPTIndexView.h"
+
+@implementation PPTIndexView
+
+
+- (inst)
+
+
+@end
