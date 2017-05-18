535e5327ed6bbe547e91cc7308d55f04e8d98080
Donald <donald@DonalddeMacBook-Air.local>
2017-05-18 16:25:15 +0800

    runloop

--- RunLoopDemo/ViewController.m
+++ RunLoopDemo/ViewController.m
@@ -16,6 +16,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    //RunLoop
+    
+    NSInvocation * action = [NSInvocation ]
+    
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
