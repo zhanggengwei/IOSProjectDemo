dfb65d12406c871264676af97225226b88ae43d9
Donald <donald@DonalddeMacBook-Air.local>
2017-05-26 15:14:42 +0800

    iOS 链式编程的实现

--- IOSRACProductDemo/ViewController.m
+++ IOSRACProductDemo/ViewController.m
@@ -7,15 +7,29 @@
 //
 
 #import "ViewController.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
 
 @interface ViewController ()
-
+@property (nonatomic,strong) UIButton * button;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.button = [[UIButton alloc]initWithFrame:CGRectMake(100, 100, 40, 40)];
+    [self.view addSubview:self.button];
+    
+    self.button.backgroundColor = [UIColor redColor];
+    
+    [[self.button rac_signalForControlEvents:UIControlEventTouchUpInside]subscribeNext:^(id x) {
+        NSLog(@"button");
+    }];
+    
+    
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
